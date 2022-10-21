fc98423e7383f486003565a42aeceb6e4808ca57
Eliot Du <ssvs1207@gmail.com>
2022-10-21 09:08:56 +0800

    1020拖拉、svg

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const serveIndex = require('serve-index');
+
 
 var app = express();
 
@@ -19,7 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
+//從public
+app.use('/',serveIndex('public',{icons:true}));
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
